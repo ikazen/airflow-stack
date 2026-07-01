f2369e27174afe96bf60fd66ccd36aa527029ccb
ikazen.nn <ikazen.nn@gmail.com>
2026-07-02 07:43:35 +0900

    chore: task image -> v1.2.6

--- dags/reflexion_rondo_cycle.py
+++ dags/reflexion_rondo_cycle.py
@@ -21,7 +21,7 @@ class DockerOperator(_DockerBase):
     template_fields = ("command", "environment")
 
 
-IMAGE = "registry.internal:5000/reflexion-rondo/task:v1.2.5"
+IMAGE = "registry.internal:5000/reflexion-rondo/task:v1.2.6"
 
 _DOCKER_BASE = dict(
     image=IMAGE,
